8d979602a66ee6e77051a8386d59401c2521e840
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2021-12-08 10:01:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, { useState } from 'react'
+import About from './Components/About';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar'
+import TextForm from './Components/TextForm';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+
+} from "react-router-dom";
 
 function App() {
+  const [mode, newMode] = useState("light")
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000)
+  }
+  const ToggleMode = () => {
+    if (mode === "light") {
+      newMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      document.body.style.color = 'white';
+      showAlert("Dark mode is enabled", "success");
+    } else {
+      newMode("light")
+      document.body.style.backgroundColor = "white"
+      document.body.style.color = 'black'
+      showAlert("Light mode is enabled", "success");
+
+    }
+  }
   return (
     <>
-   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="/">WriteMight</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/">Setting</a>
-        </li>
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-   </>
+      <Router>
+        {/* <Navbar/>  Here I set default value */}
+        
+        <Navbar title="X-text" about="About us" mode={mode} ToggleMode={ToggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-2">
+          <Switch>
+            <Route exact path="/about">
+              <About about={About}/>
+            </Route>
+            <Route exact path="/">
+              <TextForm button_head="Convert To Upper Case" mode={mode} showAlert={showAlert} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
